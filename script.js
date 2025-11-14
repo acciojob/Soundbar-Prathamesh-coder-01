8632def9e44b8f628731a7e0a86ea43c1fed1e07
Prathamesh Dabhade <prathameshdabhade71@gmail.com>
2025-11-14 16:22:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+let currentSound = null;
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const soundName = btn.getAttribute("data-sound");
+
+        if (soundName === null) {
+            stopSound();
+            return;
+        }
+
+        playSound(soundName);
+    });
+});
+
+function playSound(name) {
+    stopSound(); // Stop previously running sound
+
+    currentSound = new Audio(`sounds/${name}.mp3`);
+    currentSound.play();
+}
+
+function stopSound() {
+    if (currentSound) {
+        currentSound.pause();
+        currentSound.currentTime = 0;
+    }
+}
